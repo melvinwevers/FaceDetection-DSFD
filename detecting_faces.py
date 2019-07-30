683a54f1109b43d90b34849823389ca9b849ec56
Melvin Wevers <melvinwevers@gmail.com>
2019-07-30 10:52:49 +0200

    added script for kb-faces

---
+++ detecting_faces.py
@@ -0,0 +1,370 @@
+from __future__ import print_function
+
+import argparse
+import cv2
+from data import WIDERFace_CLASSES
+from data import *
+from face_ssd import build_ssd
+import glob
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+from torch.autograd import Variable
+
+plt.switch_backend('agg')
+
+
+parser = argparse.ArgumentParser(description="DSFD: Dual Shot Face Detector")
+parser.add_argument('--save_path', default='./eval_tools', type=str,
+                    help='Output for evaluation files')
+parser.add_argument('--trained_model',
+                    default='weights/WIDERFace_DSFD_RES152.pth',
+                    type=str, help='Trained state_dict file path to open')
+parser.add_argument('--cuda', default=True, type=bool,
+                    help='Use cuda to train model')
+parser.add_argument('--output_threshold', default='0', type=int,
+                    help='Cut-off for output')
+parser.add_argument('--annotation_dir', default='../advis/annotations',
+                    type=str, help='Dir with annotations')
+parser.add_argument('--image_set_dir', default='../advis/samples',
+                    type=str, help='Dir with images')
+parser.add_argument('--resize', default='True',
+                    type=str, help='Resize images to 1024 pixels, only if annotations are only resized')
+# TO DO: include resizing of annotations in write_txt
+
+args = parser.parse_args()
+
+if args.cuda and torch.cuda.is_available():
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+else:
+    torch.set_default_tensor_type('torch.FloatTensor')
+if not os.path.exists(SAVE_PATH):
+    os.mkdir(SAVE_PATH)
+
+
+def load_img(file_, resizing=args.resize):
+    img = cv2.imread(file_, cv2.IMREAD_COLOR)
+    h, w = img.shape[:2]
+    if resizing is True:
+        if w >= 1000 or h >= 1000:
+            print('resizing!')
+            if w > h:
+                img = image_resize(img, width=1024)
+            else:
+                img = image_resize(img, height=1024)
+    return img
+
+
+def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h, w) = image.shape[:2]
+
+    if width is None and height is None:
+        return image
+
+    if width is None:
+        r = height / float(h)
+        dim = (int(w * r), height)
+    else:
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    resized = cv2.resize(image, dim, interpolation=inter)
+    return resized
+
+
+def bbox_vote(det):
+    order = det[:, 4].ravel().argsort()[::-1]
+    det = det[order, :]
+    while det.shape[0] > 0:
+        # IOU
+        area = (det[:, 2] - det[:, 0] + 1) * (det[:, 3] - det[:, 1] + 1)
+        xx1 = np.maximum(det[0, 0], det[:, 0])
+        yy1 = np.maximum(det[0, 1], det[:, 1])
+        xx2 = np.minimum(det[0, 2], det[:, 2])
+        yy2 = np.minimum(det[0, 3], det[:, 3])
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        o = inter / (area[0] + area[:] - inter)
+        # get needed merge det and delete these det
+        merge_index = np.where(o >= 0.3)[0]
+        det_accu = det[merge_index, :]
+        det = np.delete(det, merge_index, 0)
+        if merge_index.shape[0] <= 1:
+            continue
+        det_accu[:, 0:4] = det_accu[:, 0:4] * np.tile(det_accu[:, -1:], (1, 4))
+        max_score = np.max(det_accu[:, 4])
+        det_accu_sum = np.zeros((1, 5))
+        det_accu_sum[:, 0:4] = np.sum(det_accu[:, 0:4], axis=0) / np.sum(det_accu[:, -1:])
+        det_accu_sum[:, 4] = max_score
+        try:
+            dets = np.row_stack((dets, det_accu_sum))
+        except Exception:  # if nothing is found
+            dets = det_accu_sum
+    dets = dets[0:750, :]
+    return dets
+
+
+def write_to_txt(f, det, thresh=args.output_threshold):
+    class_ = 'person'  # hard-coded
+
+    for i in range(det.shape[0]):
+        xmin = det[i][0]
+        ymin = det[i][1]
+        xmax = det[i][2]
+        ymax = det[i][3]
+        score = det[i][4]
+
+        if score > thresh:
+            f.write('{:s} {:.3f} {:.1f} {:.1f} {:.1f} {:.1f}\n'.
+                    format(class_, score, np.floor(xmin),
+                           np.floor(ymin), np.ceil(xmax + 1),
+                           np.ceil(ymax + 1)))
+
+
+def infer(net, img, transform, thresh, cuda, shrink):
+    if shrink != 1:
+        img = cv2.resize(img, None, None, fx=shrink, fy=shrink,
+                         interpolation=cv2.INTER_LINEAR)
+    x = torch.from_numpy(transform(img)[0]).permute(2, 0, 1)
+    with torch.no_grad():
+        x = x.unsqueeze(0)
+        if cuda:
+            x = x.cuda()
+            y = net(x)      # forward pass
+    detections = y.data
+    # scale each detection back up to the image
+    scale = torch.Tensor([img.shape[1]/shrink, img.shape[0]/shrink,
+                         img.shape[1]/shrink, img.shape[0]/shrink])
+    det = []
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] >= thresh:
+            score = detections[0, i, j, 0]
+            #label_name = labelmap[i-1]
+            pt = (detections[0, i, j, 1:]*scale).cpu().numpy()
+            coords = (pt[0], pt[1], pt[2], pt[3]) 
+            det.append([pt[0], pt[1], pt[2], pt[3], score])
+            j += 1
+    if (len(det)) == 0:
+        det = [ [0.1,0.1,0.2,0.2,0.01] ]
+    det = np.array(det)
+
+    keep_index = np.where(det[:, 4] >= 0)[0]
+    det = det[keep_index, :]
+    return det
+
+def infer_flip(net , img , transform , thresh , cuda , shrink):
+    img = cv2.flip(img, 1)
+    det = infer(net , img , transform , thresh , cuda , shrink)
+    det_t = np.zeros(det.shape)
+    det_t[:, 0] = img.shape[1] - det[:, 2]
+    det_t[:, 1] = det[:, 1]
+    det_t[:, 2] = img.shape[1] - det[:, 0]
+    det_t[:, 3] = det[:, 3]
+    det_t[:, 4] = det[:, 4]
+    return det_t
+
+
+def infer_multi_scale_sfd(net , img , transform , thresh , cuda ,  max_im_shrink):
+    # shrink detecting and shrink only detect big face
+    st = 0.5 if max_im_shrink >= 0.75 else 0.5 * max_im_shrink
+    det_s = infer(net , img , transform , thresh , cuda , st)
+    index = np.where(np.maximum(det_s[:, 2] - det_s[:, 0] + 1, det_s[:, 3] - det_s[:, 1] + 1) > 30)[0]
+    det_s = det_s[index, :]
+    # enlarge one times
+    bt = min(2, max_im_shrink) if max_im_shrink > 1 else (st + max_im_shrink) / 2
+    det_b = infer(net , img , transform , thresh , cuda , bt)
+    # enlarge small iamge x times for small face
+    if max_im_shrink > 2:
+        bt *= 2
+        while bt < max_im_shrink:
+            det_b = np.row_stack((det_b, infer(net , img , transform , thresh , cuda , bt)))
+            bt *= 2
+        det_b = np.row_stack((det_b, infer(net , img , transform , thresh , cuda , max_im_shrink) ))
+    # enlarge only detect small face
+    if bt > 1:
+        index = np.where(np.minimum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) < 100)[0]
+        det_b = det_b[index, :]
+    else:
+        index = np.where(np.maximum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1) > 30)[0]
+        det_b = det_b[index, :]
+    return det_s, det_b
+
+
+def detect_face(image, shrink):
+    x = image
+    if shrink != 1:
+        x = cv2.resize(image, None, None, fx=shrink, fy=shrink,
+                       interpolation=cv2.INTER_LINEAR)
+    width = x.shape[1]
+    height = x.shape[0]
+    x = x.astype(np.float32)
+    x -= np.array([104, 117, 123], dtype=np.float32)
+
+    x = torch.from_numpy(x).permute(2, 0, 1)
+    x = x.unsqueeze(0)
+    with torch.no_grad():
+        x = x.cuda()
+        y = net(x)
+    detections = y.data
+    scale = torch.Tensor([width, height, width, height])
+
+    boxes = []
+    scores = []
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] >= 0.01:
+            score = detections[0, i, j, 0]
+            pt = (detections[0, i, j, 1:] * scale).cpu().numpy()
+            boxes.append([pt[0], pt[1], pt[2], pt[3]])
+            scores.append(score)
+            j += 1
+            if j >= detections.size(2):
+                break
+
+    det_conf = np.array(scores)
+    boxes = np.array(boxes)
+
+    if boxes.shape[0] == 0:
+        return np.array([[0, 0, 0, 0, 0.001]])
+
+    det_xmin = boxes[:, 0] / shrink
+    det_ymin = boxes[:, 1] / shrink
+    det_xmax = boxes[:, 2] / shrink
+    det_ymax = boxes[:, 3] / shrink
+    det = np.column_stack((det_xmin, det_ymin, det_xmax, det_ymax, det_conf))
+
+    keep_index = np.where(det[:, 4] >= 0)[0]
+    det = det[keep_index, :]
+    return det
+
+
+def multi_scale_test(image, max_im_shrink):
+    # shrink detecting and shrink only detect big face
+    st = 0.5 if max_im_shrink >= 0.75 else 0.5 * max_im_shrink
+    det_s = detect_face(image, st)
+    if max_im_shrink > 0.75:
+        det_s = np.row_stack((det_s, detect_face(image, 0.75)))
+    index = np.where(np.maximum(det_s[:, 2] - det_s[:, 0] + 1,
+                                det_s[:, 3] - det_s[:, 1] + 1) > 30)[0]
+    det_s = det_s[index, :]
+    # enlarge one times
+    bt = min(2, max_im_shrink) if max_im_shrink > 1 else (st + max_im_shrink) / 2
+    det_b = detect_face(image, bt)
+
+    # enlarge small iamge x times for small face
+    if max_im_shrink > 1.5:
+        det_b = np.row_stack((det_b, detect_face(image,1.5)))
+    if max_im_shrink > 2:
+        bt *= 2
+        while bt < max_im_shrink:  # and bt <= 2:
+            det_b = np.row_stack((det_b, detect_face(image, bt)))
+            bt *= 2
+
+        det_b = np.row_stack((det_b, detect_face(image, max_im_shrink)))
+
+    # enlarge only detect small face
+    if bt > 1:
+        index = np.where(np.minimum(det_b[:, 2] - det_b[:, 0] + 1,
+                                    det_b[:, 3] - det_b[:, 1] + 1) < 100)[0]
+        det_b = det_b[index, :]
+    else:
+        index = np.where(np.maximum(det_b[:, 2] - det_b[:, 0] + 1,
+                                    det_b[:, 3] - det_b[:, 1] + 1) > 30)[0]
+        det_b = det_b[index, :]
+
+    return det_s, det_b
+
+def multi_scale_test_pyramid(image, max_shrink):
+    # shrink detecting and shrink only detect big face
+    det_b = detect_face(image, 0.25)
+    index = np.where(
+        np.maximum(det_b[:, 2] - det_b[:, 0] + 1, det_b[:, 3] - det_b[:, 1] + 1)
+        > 30)[0]
+    det_b = det_b[index, :]
+
+    st = [1.25, 1.75, 2.25]
+    for i in range(len(st)):
+        if (st[i] <= max_shrink):
+            det_temp = detect_face(image, st[i])
+            # enlarge only detect small face
+            if st[i] > 1:
+                index = np.where(
+                    np.minimum(det_temp[:, 2] - det_temp[:, 0] + 1,
+                               det_temp[:, 3] - det_temp[:, 1] + 1) < 100)[0]
+                det_temp = det_temp[index, :]
+            else:
+                index = np.where(
+                    np.maximum(det_temp[:, 2] - det_temp[:, 0] + 1,
+                               det_temp[:, 3] - det_temp[:, 1] + 1) > 30)[0]
+                det_temp = det_temp[index, :]
+            det_b = np.row_stack((det_b, det_temp))
+    return det_b
+
+
+def flip_test(image, shrink):
+    image_f = cv2.flip(image, 1)
+    det_f = detect_face(image_f, shrink)
+
+    det_t = np.zeros(det_f.shape)
+    det_t[:, 0] = image.shape[1] - det_f[:, 2]
+    det_t[:, 1] = det_f[:, 1]
+    det_t[:, 2] = image.shape[1] - det_f[:, 0]
+    det_t[:, 3] = det_f[:, 3]
+    det_t[:, 4] = det_f[:, 4]
+    return det_t
+
+
+def detect_kb_faces():
+    # evaluation
+    save_path = args.save_path
+    annotations = glob.glob(args.annotation_dir + '/*')  # load annotation xml
+    image_set = glob.glob(args.image_set_dir + '/*')  # load image files
+
+    annotations_base = [os.path.splitext(os.path.basename(annotation))[0]
+                        for annotation in annotations]
+
+    for i in range(0, len(image_set)):
+        img_id = os.path.splitext(os.path.basename(image_set[i]))[0]
+        if img_id in annotations_base:
+            image = load_img(image_set[i])
+
+            print('Detecting Faces in image {:d}/{:d} {}....'.format(i+1, num_images , img_id))
+
+            max_im_shrink = (0x7fffffff / 200.0 / (image.shape[0] * image.shape[1])) ** 0.5  # the max size of input image for caffe
+            max_im_shrink = 3 if max_im_shrink > 3 else max_im_shrink
+
+            shrink = max_im_shrink if max_im_shrink < 1 else 1
+
+            det0 = detect_face(image, shrink)  # origin test
+            det1 = flip_test(image, shrink)    # flip test
+            [det2, det3] = multi_scale_test(image, max_im_shrink)  # multi-scale test
+            det4 = multi_scale_test_pyramid(image, max_im_shrink)
+            det = np.row_stack((det0, det1, det2, det3, det4))
+
+            try:
+                det = bbox_vote(det)
+                if not os.path.exists(save_path):
+                    os.makedirs(save_path)
+                f = open(save_path + '/' + img_id.split(".")[0] + '.txt', 'w')
+                write_to_txt(f, det)
+            except Exception:
+                print('Nothing Found')
+        else:
+            pass
+
+
+if __name__ == '__main__':
+    cfg = widerface_640
+    num_classes = len(WIDERFace_CLASSES) + 1  # +1 background
+    net = build_ssd('test', cfg['min_dim'], num_classes)  # initialize SSD
+    net.load_state_dict(torch.load(args.trained_model))
+    net.cuda()
+    net.eval()
+    print('Finished loading model!')
+
+    detect_kb_faces()
+
